4940cbb45cd3ee5bdc12547ef2f66080e1910248
Aliaksandr Dvornik <aadvornik@yandex.ru>
2021-03-22 11:55:14 +0300

    Done!

--- Tasks/src/Exercise06.java
+++ Tasks/src/Exercise06.java
@@ -3,20 +3,23 @@ public class Exercise06 {
     public static void main(String[] args) {
         double[] num = {0.2, 4, 1.8, 2, 4.3, 4.5, 8, 5, 6.9, 3};
         int sum = 0;
-        for (double i : num) {
-
-            for (int j = 2; j < i; i++) {
-                if (i % j == 0) {
-
-                }
-
-
-            /*int n = 2;
-            if(num[(int) i] % n == 0);
-            sum += num[(int)i];
-            n++;*/
+        for (int i = 0; i< num.length; i++) {
+           if(isPrime(i)){
+                sum+=num[i];
             }
-            // System.out.println(sum);
         }
+        System.out.println(sum);
+    }
+
+    public static boolean isPrime(int value){
+        if(value<2){
+            return false;
+        } else {
+            for(int i = 2; i<= value/2; i++){
+                if(value%i == 0){
+                    return false;
+                }
+            }
+        } return true;
     }
 }
