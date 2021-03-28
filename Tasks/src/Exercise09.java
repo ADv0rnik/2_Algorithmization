c98f737c6d3b4318266f98c33d6aa747642cf320
Aliaksandr Dvornik <aadvornik@gmail.com>
2021-03-28 18:30:19 +0300

    first commit for Ex9

---
+++ Tasks/src/Exercise09.java
@@ -0,0 +1,16 @@
+public class Exercise09 {
+    public static void main(String[] args) {
+        int n = 10;
+        int[]num = new int[n];
+        for(int i: num) {
+            num[i] = (int) (Math.random() * 10);
+            System.out.println(num[i]);
+        }
+
+        for(int i: num){
+
+        }
+    }
+
+
+}
