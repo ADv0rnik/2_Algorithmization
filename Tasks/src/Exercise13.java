1f99001f4b8adb5919d2c2db9a390ddd3764b34a
Aliaksandr Dvornik <aadvornik@gmail.com>
2021-04-02 01:17:06 +0300

    Done!

--- Tasks/src/Exercise13.java
+++ Tasks/src/Exercise13.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+//A matrix is given. Print the k-row and p-column of the matrix
 public class Exercise13 {
     public static void main(String[] args) {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
@@ -18,11 +19,19 @@ public class Exercise13 {
                 System.out.println();
             }
             System.out.println();
-            for(int i= 0; i<twoDimArray.length; i++){
-                for (int j = 0; j< twoDimArray.length; j++){
-                    System.out.print(" "+ twoDimArray[i][j] + " ");
+            if((k > twoDimArray.length)||(k < 0)||(p > twoDimArray.length)||(p < 0)){
+                System.out.println("The line does not exist in the matrix");
+            } else {
+                System.out.println("k-line:");
+                for(int i = 0; i<twoDimArray.length; i++){
+                    System.out.print(" "+twoDimArray[k][i]+" ");
                 }
                 System.out.println();
+                System.out.println("p-column:");
+                for (int j = 0; j< twoDimArray.length; j++){
+                    System.out.print(" "+twoDimArray[j][p]+" ");
+                    System.out.println();
+                }
             }
         } catch (IOException e) {
             System.out.println("Error: "+ e.toString());
