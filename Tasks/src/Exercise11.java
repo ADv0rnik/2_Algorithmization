81efff652a36e2daa0e625c11f95b166a01d6341
Aliaksandr Dvornik <aadvornik@gmail.com>
2021-04-01 23:30:48 +0300

    Done!

--- Tasks/src/Exercise11.java
+++ Tasks/src/Exercise11.java
@@ -1,11 +1,22 @@
+//A matrix is given. Display all odd columns with the first item greater than the last.
 public class Exercise11 {
     public static void main(String[] args) {
         int[][]twoDimArray = {{5,7,3,17}, {7,0,1,12}, {8,1,2,3}};
+        System.out.println("The matrix: ");
         for(int i = 0; i<twoDimArray.length; i++){
             for (int j=0; j< twoDimArray[0].length; j++){
+                    System.out.print(" "+twoDimArray[i][j]+" ");
+                }
+            System.out.println();
+        }
+        System.out.println("Desired column: ");
+        for(int i = 0; i<twoDimArray.length; i++){
+            for (int j=0; j< twoDimArray[0].length; j+=2){
+                if(twoDimArray[0][j] > twoDimArray[twoDimArray.length-1][j])
                 System.out.print(" "+twoDimArray[i][j]+" ");
             }
             System.out.println();
         }
+
     }
 }
